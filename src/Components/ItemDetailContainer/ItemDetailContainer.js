1ea4cb5c22abebac8bd8d67952f7a58616fb08ce
Luciano Fernandez <fer.luciano1987@gmail.com>
2021-05-02 01:30:53 -0300

    corrigiendo rutas de img

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -44,7 +44,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 4,
-                        img: "./venzo-atix.jpeg",
+                        img: "../venzo-atix.jpeg",
                         description: "R29 - 2x10v - Full Shimano Deore",
                         tittle: "Venzo Atix - 2x10v",
                         price:" 140000",
@@ -54,7 +54,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 5,
-                        img: "./venzo-primal.jpeg",
+                        img: "../venzo-primal.jpeg",
                         description: "R29 - 3x8 - 24v, Shimano Altus, frenos a disco hidráulicos",
                         tittle: "Venzo PRIMAL XC Shadow",
                         price:" 75000",
@@ -64,7 +64,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 6,
-                        img: "./venzo-raptor.jpeg",
+                        img: "../venzo-raptor.jpeg",
                         description: "R29 - 3x8 - Shimano Altus, frenos a disco hidráulicos",
                         tittle: "Venzo RAPTOR EVO",
                         price:" 130000",
@@ -74,7 +74,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 7,
-                        img: "./vairo-3.5.jpg",
+                        img: "../vairo-3.5.jpg",
                         description: "R29 - XR 3.5 - Bicicleta MountainBike de Aluminio Rodado 29, Frenos a Disco, Suspensión C/Bloqueo, Llantas Doble Pared, Full Shimano.",
                         tittle: "Nueva VAIRO XR 3.5 2021",
                         price:" 130000",
@@ -84,7 +84,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 8,
-                        img: "./vairo-3.8.jpg",
+                        img: "../vairo-3.8.jpg",
                         description: "R29 - 3x8 - Freno a Disco Shimano / Tektro",
                         tittle: "Nueva VAIRO XR 3.8 29",
                         price:" 130000",
@@ -94,7 +94,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 9,
-                        img: "./vairo-5.0.jpg",
+                        img: "../vairo-5.0.jpg",
                         description: "R29 - XR 5.0 - Aluminio 6061T6,Freno A disco, Hidráulico – Shimano/Tektro",
                         tittle: "Nueva VAIRO 5.0",
                         price:" 150000",
@@ -104,7 +104,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 10,
-                        img: "./Accesorios/venzo-casco.png",
+                        img: "../Accesorios/venzo-casco.png",
                         description: "Con regulación y vicera - Talles M / L - Colores: negro/rojo, negro/verde, negro/azul",
                         tittle: "Casco Modelo 017",
                         price:" 5000",
@@ -114,7 +114,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 11,
-                        img: "./Accesorios/venzo-casco02.png",
+                        img: "../Accesorios/venzo-casco02.png",
                         description: "Con regulación y vicera - Talles M / L - Colores: negro/rojo, negro/verde, negro/azul",
                         tittle: "Casco Modelo 010",
                         price:" 5500",
@@ -124,7 +124,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 12,
-                        img: "./Accesorios/venzo-asiento.png",
+                        img: "../Accesorios/venzo-asiento.png",
                         description: "Rieles cuero sintetico - Tamaño 270 x 145 mm",
                         tittle: "Asiento VENZO MX",
                         price:" 6000",
@@ -134,7 +134,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 13,
-                        img: "./Accesorios/venzo-balizas.png",
+                        img: "../Accesorios/venzo-balizas.png",
                         description: "5 luces led - 3 funciones- Modo de carga 2 pillas AAA",
                         tittle: "Baliza VENZO 001-008",
                         price:" 3500",
@@ -144,7 +144,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 14,
-                        img: "./Accesorios/venzo-farol.png",
+                        img: "../Accesorios/venzo-farol.png",
                         description: "1 luz led - 3 funciones - Carcaza de aluminio - Resistente a la lluvia",
                         tittle: "Farol delantero VENZO F01-006",
                         price:" 5500",
@@ -154,7 +154,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 15,
-                        img: "./Accesorios/venzo-gomaEspuma.png",
+                        img: "../Accesorios/venzo-gomaEspuma.png",
                         description: "Largo 128 mm",
                         tittle: "Puños VENZO de goma espuma E05-",
                         price:" 2500",
@@ -164,7 +164,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 16,
-                        img: "./Accesorios/venzo-guantes-cortos.png",
+                        img: "../Accesorios/venzo-guantes-cortos.png",
                         description: "Dedos cortos - Con abrojo y almohadillas - Talles S/M/L/XL",
                         tittle: "Guantes modelo 003",
                         price:" 6000",
@@ -174,7 +174,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 17,
-                        img: "./Accesorios/venzo-guantes-largos.png",
+                        img: "../Accesorios/venzo-guantes-largos.png",
                         description: "Dedos largos - Con abrojo y almohadillas - Talles S/M/L/XL",
                         tittle: "Guantes modelo 001",
                         price:" 150000",
@@ -184,7 +184,7 @@ const ItemDetailContainer = () => {
                     }, 
                     {
                         id: 18,
-                        img: "./Accesorios/venzo-lentes-rojos.png",
+                        img: "../Accesorios/venzo-lentes-rojos.png",
                         description: "2 lentes cambiables - Proteccion UV",
                         tittle: "Lentes modelo 001",
                         price:" 8000",
@@ -194,7 +194,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 19,
-                        img: "./Accesorios/venzo-lentes.png",
+                        img: "../Accesorios/venzo-lentes.png",
                         description: "2 lentes cambiables - Proteccion UV",
                         tittle: "Lentes modelo 002",
                         price:" 8000",
@@ -204,7 +204,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 20,
-                        img: "./Accesorios/venzo-llave.png",
+                        img: "../Accesorios/venzo-llave.png",
                         description: "Lllaves allen medidas 2/2.5/3/4/5/6/8 - Saca cubiertas - Llaves de tubo 8/9/10/12",
                         tittle: "Herramienta VENZO F33 002-13 funciones",
                         price:" 4000",
@@ -214,7 +214,7 @@ const ItemDetailContainer = () => {
                     },
                     {
                         id: 21,
-                        img: "./Accesorios/venzo-portasilla.png",
+                        img: "../Accesorios/venzo-portasilla.png",
                         description: "Material fibra de carbono - Diametro 31.6 - Longitud 350mm",
                         tittle: "Portasilla VENZO XTV",
                         price:" 7000",
